4d24eb9b65fbc81a2c2a2e5f13651153d33361fe
liamkinne <kinneliam@gmail.com>
2017-12-11 18:37:35 +1100

    added avr parameters

--- Pin.cpp
+++ Pin.cpp
@@ -2,9 +2,15 @@
 
 using namespace Bubble;
 
-Pin::Pin(uint8_t number)
+Pin::Pin(uint8_t number): number(number)
 {
-
+	port			= pgm_read_byte(digitalPinToPortPGM + (number));
+	bitMask			= pgm_read_byte(digitalPinToBitMaskPGM + (number));
+	timer			= pgm_read_byte(digitalPinToTimerPGM + (number) );
+	bit				= number;
+	inputRegister	= (volatile uint8_t *)(pgm_read_word(portToOutputPGM + (number)));
+	outputRegister	= (volatile uint8_t *)(pgm_read_word(portToInputPGM + (number)));
+	modeRegister	= (volatile uint8_t *)(pgm_read_word(portToModePGM + (number)));
 }
 
 Pin::~Pin()
--- Pin.h
+++ Pin.h
@@ -1,6 +1,8 @@
 #ifndef PIN_H
 #define PIN_H
 
+#include <avr/pgmspace.h>
+
 namespace Bubble
 {
 	class Pin
@@ -8,9 +10,19 @@ namespace Bubble
 	public:
 		Pin(uint8_t number);
 		~Pin();
-	
+		
+		uint8_t getNumber() {return number;}
+		uint8_t getPort() {return port;}
+		uint8_t getBitMask() {return bitMask;}
+		uint8_t getTimer() {return timer;}
+		uint8_t getBit() {return analogBit;} // Only for analog pins
+		uint8_t getInputRegister() {return inputRegister;}
+		uint8_t getOutputRegister() {return outputRegister;}
+		uint8_t getModeRegister() {return modeRegister;}
 	private:
 		uint8_t number;
+		// AVR
+		uint8_t port, bitMask, timer, analogBit, inputRegister, outputRegister, modeRegister;
 	};
 }
 
