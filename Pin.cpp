66d749e07b8800512e1a2045a27c965a1edf566c
liamkinne <kinneliam@gmail.com>
2017-12-11 18:46:46 +1100

    formatting fix

--- Pin.cpp
+++ Pin.cpp
@@ -6,7 +6,7 @@ Pin::Pin(uint8_t number): number(number)
 {
 	port			= pgm_read_byte(digitalPinToPortPGM + (number));
 	bitMask			= pgm_read_byte(digitalPinToBitMaskPGM + (number));
-	timer			= pgm_read_byte(digitalPinToTimerPGM + (number) );
+	timer			= pgm_read_byte(digitalPinToTimerPGM + (number));
 	analogBit		= number;
 	modeRegister	= (volatile uint8_t *)(pgm_read_word(portToModePGM + (number)));
 	inputRegister	= (volatile uint8_t *)(pgm_read_word(portToOutputPGM + (number)));
