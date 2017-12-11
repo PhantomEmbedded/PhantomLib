6fe5d8361bb4531155338c1ffc5c0fa782d4e4c7
liamkinne <kinneliam@gmail.com>
2017-12-11 19:00:34 +1100

    Added port mode setting to constructor

--- DigitalInput.cpp
+++ DigitalInput.cpp
@@ -2,9 +2,33 @@
 
 using namespace Phantom;
 
-DigitalInput::DigitalInput(Pin &pin): pin(pin)
+DigitalInput::DigitalInput(Pin &pin, bool pullup): pin(pin)
 {
+	uint8_t port = pin.getPort();
+	uint8_t bit = pin.getBitMask();
 
+	if (port == 0) return; // Check if valid
+
+	volatile uint8_t *reg, *out;
+
+	reg = port.getModeRegister();
+	out = port,getOutputRegister();
+
+	if (pullup)
+	{ 
+		uint8_t oldSREG = SREG;
+				cli();
+		*reg &= ~bit;
+		*out |= bit;
+	}
+	else
+	{
+		uint8_t oldSREG = SREG;
+				cli();
+		*reg &= ~bit;
+		*out &= ~bit;
+		SREG = oldSREG;
+	}
 }
 
 DigitalInput::~DigitalInput()
