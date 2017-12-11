f951a7f5d74c9a8ca7738565e5033ba6f82919d7
liamkinne <kinneliam@gmail.com>
2017-12-11 20:19:44 +1100

    Added Math

---
+++ Math.h
@@ -0,0 +1,9 @@
+#ifndef MATH_H
+#define MATH_H
+
+template<typename T, typename T2>
+inline T map(T2 val, T2 in_min, T2 in_max, T out_min, T out_max) {
+		return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+#endif /* END MATH_H */
