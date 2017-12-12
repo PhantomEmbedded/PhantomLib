bbebc4de87ebd3cda7ba8b7db0359f3b8d62ce8a
liamkinne <kinneliam@gmail.com>
2017-12-12 16:06:09 +1100

    port as input

--- Pin.cpp
+++ Pin.cpp
@@ -8,9 +8,9 @@ Pin::Pin(uint8_t number): number(number)
 	bitMask			= pgm_read_byte(digitalPinToBitMaskPGM + (number));
 	timer			= pgm_read_byte(digitalPinToTimerPGM + (number));
 	analogBit		= number;
-	modeRegister	= (volatile uint8_t *)(pgm_read_word(portToModePGM + (number)));
-	inputRegister	= (volatile uint8_t *)(pgm_read_word(portToOutputPGM + (number)));
-	outputRegister	= (volatile uint8_t *)(pgm_read_word(portToInputPGM + (number)));
+	modeRegister	= (volatile uint8_t *)(pgm_read_word(portToModePGM + (port)));
+	inputRegister	= (volatile uint8_t *)(pgm_read_word(portToOutputPGM + (port)));
+	outputRegister	= (volatile uint8_t *)(pgm_read_word(portToInputPGM + (port)));
 }
 
 Pin::~Pin()
