c143a8eeb14d7aa36781053f5602876e0f979984
liamkinne <kinneliam@gmail.com>
2017-12-14 18:07:33 +1100

    Fixed input and output being swapped

--- Pin.cpp
+++ Pin.cpp
@@ -9,8 +9,8 @@ Pin::Pin(uint8_t number): number(number)
 	timer			= pgm_read_byte(digitalPinToTimerPGM + (number));
 	analogBit		= number;
 	modeRegister	= (volatile uint16_t *)(pgm_read_word(portToModePGM + (port)));
-	inputRegister	= (volatile uint16_t *)(pgm_read_word(portToOutputPGM + (port)));
-	outputRegister	= (volatile uint16_t *)(pgm_read_word(portToInputPGM + (port)));
+	inputRegister	= (volatile uint16_t *)(pgm_read_word(portToInputPGM + (port)));
+	outputRegister	= (volatile uint16_t *)(pgm_read_word(portToOutputPGM + (port)));
 }
 
 Pin::~Pin()
