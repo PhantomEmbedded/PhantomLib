3a01dc69adf57771cc66fd92862bba708c3d9624
liamkinne <kinneliam@gmail.com>
2017-12-16 23:22:06 +1100

    Change macros to constants

--- Boards/BoardBase.h
+++ Boards/BoardBase.h
@@ -1,42 +1,45 @@
 #ifndef BOARD_BASE
 #define BOARD_BASE
 
-#define NOT_A_PIN 0
-#define NOT_A_PORT 0
+#include <avr/pgmspace.h>
+#include "../Phantom.h"
 
-#define NOT_AN_INTERRUPT -1
+const uint8_t NOT_A_PIN = 0;
+const uint8_t NOT_A_PORT = 0;
 
-#define PA 1
-#define PB 2
-#define PC 3
-#define PD 4
-#define PE 5
-#define PF 6
-#define PG 7
-#define PH 8
-#define PJ 10
-#define PK 11
-#define PL 12
+const uint8_t NOT_AN_INTERRUPT = -1;
 
-#define NOT_ON_TIMER 0
-#define TIMER0A 1
-#define TIMER0B 2
-#define TIMER1A 3
-#define TIMER1B 4
-#define TIMER1C 5
-#define TIMER2  6
-#define TIMER2A 7
-#define TIMER2B 8
-#define TIMER3A 9
-#define TIMER3B 10
-#define TIMER3C 11
-#define TIMER4A 12
-#define TIMER4B 13
-#define TIMER4C 14
-#define TIMER4D 15
-#define TIMER5A 16
-#define TIMER5B 17
-#define TIMER5C 18
+const uint8_t PA = 1;
+const uint8_t PB = 2;
+const uint8_t PC = 3;
+const uint8_t PD = 4;
+const uint8_t PE = 5;
+const uint8_t PF = 6;
+const uint8_t PG = 7;
+const uint8_t PH = 8;
+const uint8_t PJ = 10;
+const uint8_t PK = 11;
+const uint8_t PL = 12;
+
+const uint8_t NOT_ON_TIMER = 0;
+const uint8_t TIMER0A = 1;
+const uint8_t TIMER0B = 2;
+const uint8_t TIMER1A = 3;
+const uint8_t TIMER1B = 4;
+const uint8_t TIMER1C = 5;
+const uint8_t TIMER2  = 6;
+const uint8_t TIMER2A = 7;
+const uint8_t TIMER2B = 8;
+const uint8_t TIMER3A = 9;
+const uint8_t TIMER3B = 10;
+const uint8_t TIMER3C = 11;
+const uint8_t TIMER4A = 12;
+const uint8_t TIMER4B = 13;
+const uint8_t TIMER4C = 14;
+const uint8_t TIMER4D = 15;
+const uint8_t TIMER5A = 16;
+const uint8_t TIMER5B = 17;
+const uint8_t TIMER5C = 18;
 
 // External arrays defined in board header
 extern const uint16_t portToModePGM[];
