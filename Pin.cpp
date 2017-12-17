57d113af420cfd063608d9c1130774b1f14c07cc
liamkinne <kinneliam@gmail.com>
2017-12-17 17:52:22 +1100

    Cast int to pointer properly

--- Pin.cpp
+++ Pin.cpp
@@ -6,7 +6,7 @@ Pin::Pin(uint8_t number): number(number)
 {
 	port			= pgm_read_byte(digitalPinToPortPGM + (number));
 	bitMask			= pgm_read_byte(digitalPinToBitMaskPGM + (number));
-	timer			= (volatile uint16_t *)(pgm_read_byte(digitalPinToTimerPGM + number));
+	timer			= (volatile uint16_t *)(pgm_read_byte(digitalPinToTimerPGM + (intptr_t)number));
 	analogBit		= number;
 	modeRegister	= (volatile uint16_t *)(pgm_read_word(portToModePGM + port));
 	inputRegister	= (volatile uint16_t *)(pgm_read_word(portToInputPGM + port));
