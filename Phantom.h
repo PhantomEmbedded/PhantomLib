96625695f1d336263b06cf0259c8c73113296448
liamkinne <kinneliam@gmail.com>
2017-12-17 17:53:43 +1100

    Added delete

--- Phantom.h
+++ Phantom.h
@@ -11,4 +11,12 @@ void *operator new[](size_t size) {
   return malloc(size);
 }
 
+void operator delete(void * ptr) {
+  free(ptr);
+}
+
+void operator delete[](void * ptr) {
+  free(ptr);
+}
+
 #endif /* END PHANTOM_H */
