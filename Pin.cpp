2a2cd47baa1b5e70ad6f29e197e72f752437b43b
liamkinne <kinneliam@gmail.com>
2017-12-17 17:54:10 +1100

    Put in right order

--- Pin.cpp
+++ Pin.cpp
@@ -6,8 +6,8 @@ Pin::Pin(uint8_t number): number(number)
 {
 	port			= pgm_read_byte(digitalPinToPortPGM + (number));
 	bitMask			= pgm_read_byte(digitalPinToBitMaskPGM + (number));
-	timer			= (volatile uint16_t *)(pgm_read_byte(digitalPinToTimerPGM + (intptr_t)number));
 	analogBit		= number;
+	timer			= (volatile uint16_t *)(pgm_read_byte(digitalPinToTimerPGM + (intptr_t)number));
 	modeRegister	= (volatile uint16_t *)(pgm_read_word(portToModePGM + port));
 	inputRegister	= (volatile uint16_t *)(pgm_read_word(portToInputPGM + port));
 	outputRegister	= (volatile uint16_t *)(pgm_read_word(portToOutputPGM + port));
