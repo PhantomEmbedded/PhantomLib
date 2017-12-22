cc152d8f15a872e148ac708efb6b367c2bfe995b
liamkinne <kinneliam@gmail.com>
2017-12-22 14:13:37 +1100

    Added comments

--- DigitalOutput.cpp
+++ DigitalOutput.cpp
@@ -12,7 +12,13 @@ DigitalOutput::DigitalOutput(Pin *pin): pin(pin)
 
 DigitalOutput::~DigitalOutput()
 {
+	// The order of these events ensures that the output is driven from
+	// high -> low, rather than high -> internal pullup -> low.
+	
+	// Drive output low
 	set(false);
+
+	// Set mode to input
 	auto bit = pin->getBitMask();
 	auto mode = pin->getModeRegister();
 	*mode &= ~bit;
@@ -24,26 +30,22 @@ inline void DigitalOutput::set(bool state)
 	auto bit = pin->getBitMask();
 	auto out = pin->getOutputRegister();
 
-	if (state) {
-		*out |= bit;
-	} else {
-		*out &= ~bit;
-	}
+	if (state)
+		*out |= bit; // high
+	else
+		*out &= ~bit; // low
 }
 
 inline void DigitalOutput::toggle()
 {
 	auto bit = pin->getBitMask();
 	auto out = pin->getOutputRegister();
-
-	*out ^= bit;
+	*out ^= bit; // Toggle state
 }
 
 inline void DigitalOutput::pulse(double time, bool state)
 {
 	set(state);
-
 	_delay_us(time);
-
 	set(!state);
 }
