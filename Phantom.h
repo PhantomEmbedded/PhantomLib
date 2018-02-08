16dd78d4d33ff85e0dcbcb58a27f56b2a3555d66
liamkinne <kinneliam@gmail.com>
2018-02-08 19:15:56 +1100

    add operator function prototypes

--- Phantom.h
+++ Phantom.h
@@ -3,4 +3,9 @@
 
 #include <stdlib.h>
 
+void *operator new(size_t size);
+void *operator new[](size_t size);
+void operator delete(void * ptr);
+void operator delete[](void * ptr);
+
 #endif /* END PHANTOM_H */
