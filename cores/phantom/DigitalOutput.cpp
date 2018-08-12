91749e6121a6a014ca31db31b201a5db973212e9
Liam Kinne <kinneliam@gmail.com>
2018-08-12 19:37:05 +1000

    remove fluff

--- cores/phantom/DigitalOutput.cpp
+++ cores/phantom/DigitalOutput.cpp
@@ -2,30 +2,20 @@
 
 DigitalOutput::DigitalOutput(GPIO::Port port, uint8_t pin):
 	port(port),
-	pin(pin),
 	bitmask(1<<pin)
-{
-
-	initialize();
-}
-
-void DigitalOutput::initialize()
 {
 	*port.mode_register |= bitmask;
 }
 
-void DigitalOutput::set(State state)
+void DigitalOutput::set(bool state)
 {
-	if (state == High)
+	if (state)
 		*port.output_register |= bitmask;
 	else
 		*port.output_register &= ~bitmask;
 }
 
-void DigitalOutput::set(bool state) {
-	set((State)state);
-}
-
-void DigitalOutput::toggle() {
+void DigitalOutput::toggle()
+{
 	*port.input_register |= bitmask;
 }
--- cores/phantom/DigitalOutput.h
+++ cores/phantom/DigitalOutput.h
@@ -6,22 +6,13 @@
 class DigitalOutput
 {
 public:
-	enum State {
-		High = true,
-		Low = false
-	};
-
 	DigitalOutput(GPIO::Port port, uint8_t pin);
 
-	void initialize();
-
-	void set(State state);
 	void set(bool state);
 	void toggle();
 
 private:
 	const GPIO::Port port;
-	const uint8_t pin;
 	const uint8_t bitmask;
 };
 
