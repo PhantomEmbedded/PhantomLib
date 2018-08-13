cdd18a1418b5387329644511ccfb5b59b8b1b7e8
Liam Kinne <kinneliam@gmail.com>
2018-08-13 16:50:18 +1000

    add mks base variant

---
+++ variants/mks_base/GPIO.h
@@ -0,0 +1,22 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <avr/io.h>
+#include <GPIOBase.h>
+
+namespace GPIO
+{
+	const Port A = {&PINA,&DDRA,&PORTA};
+	const Port B = {&PINB,&DDRB,&PORTB};
+	const Port C = {&PINC,&DDRC,&PORTC};
+	const Port D = {&PIND,&DDRD,&PORTD};
+	const Port E = {&PINE,&DDRE,&PORTE};
+	const Port F = {&PINF,&DDRF,&PORTF};
+	const Port G = {&PING,&DDRG,&PORTG};
+	const Port H = {&PINH,&DDRH,&PORTH};
+	const Port J = {&PINJ,&DDRJ,&PORTJ};
+	const Port K = {&PINK,&DDRK,&PORTK};
+	const Port L = {&PINL,&DDRL,&PORTL};
+}
+
+#endif
