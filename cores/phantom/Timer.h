0893d33336c197431de706e4b93f6e65f32e5a2b
Liam Kinne <kinneliam@gmail.com>
2018-08-16 00:19:33 +1000

    add 16 bit timer and PROGMEM stuff

--- cores/phantom/Timer.h
+++ cores/phantom/Timer.h
@@ -80,6 +80,83 @@ namespace Timer
 			volatile uint8_t * full;
 		} TIFR0n;
 	} TimerCounter8BitWithPWM;
+
+	typedef struct 
+	{
+		union
+		{
+			struct
+			{
+				volatile uint8_t
+						WGMn0 	: 1,
+						WGMn1 	: 1,
+						unused 	: 2,
+						COMnB0	: 1,
+						COMnB1	: 1,
+						COMnA0	: 1,
+						COMnA1	: 1;
+			};
+			volatile uint8_t * full;
+		} TCCRnA;
+		union
+		{
+			struct
+			{
+				volatile uint8_t
+						CSn 	: 3,
+						WGMn2 	: 1,
+						WGMn3 	: 1,
+						unused	: 1,
+						ICES	: 1,
+						ICNC	: 1;
+			};
+			volatile uint8_t * full;
+		} TCCRnB;
+		union
+		{
+			struct
+			{
+				volatile uint8_t
+						unused	: 6,
+						FOCnA	: 1,
+						FOCnB	: 1;
+			};
+		} TCCRnC;
+		volatile uint8_t * TCNTnL;
+		volatile uint8_t * TCNTnH;
+		volatile uint8_t * ICRnL;
+		volatile uint8_t * ICRnH;
+		volatile uint8_t * OCRnAL;
+		volatile uint8_t * OCRnAH;
+		volatile uint8_t * OCRnBL;
+		volatile uint8_t * OCRnBH;
+		union
+		{
+			struct
+			{
+				volatile uint8_t
+						TOIE	: 1,
+						OCIEA	: 1,
+						OCIEB	: 1,
+						unused	: 2,
+						ICIE	: 1;
+			};
+			volatile uint8_t * full;
+		} TIMSKn;
+		union
+		{
+			struct
+			{
+				volatile uint8_t
+						TOV		: 1,
+						OCFA	: 1,
+						OCFB	: 1,
+						unused	: 2,
+						ICF		: 1;
+			};
+			volatile uint8_t * full;
+		} TIFRn;
+	} TimerCounter16BitWithPWM;
 }
 
 #endif
--- variants/uno/Timers.h
+++ variants/uno/Timers.h
@@ -1,4 +1,6 @@
 #include <Timer.h>
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 
-Timer::TimerCounter8BitWithPWM TC0 = {TCCR0A, TCCR0B, TIMSK0, AVR_GTCCR, TCNT0, OCR0A, OCR0B, TIFR0};
+const PROGMEM Timer::TimerCounter8BitWithPWM TC0 = {&TCCR0A, &TCCR0B, &TIMSK0, &AVR_GTCCR, &TCNT0, &OCR0A, &OCR0B, &TIFR0};
+const PROGMEM Timer::TimerCounter8BitWithPWM TC1 = {&TCCR1A, &TCCR1B, &TCCR1C, &TCNT1L, &TCNT1H, &ICR1L, &ICR1H, &OCR1AL, &OCR1AH, &OCR1BL, &OCR1BH, &TIMSK1, &TIFR1};
