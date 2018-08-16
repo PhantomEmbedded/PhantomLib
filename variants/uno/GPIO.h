daaf0794cbc69a02d18b468f5e2651c9cb5d730c
Liam Kinne <kinneliam@gmail.com>
2018-08-16 10:13:07 +1000

    putting addresses in progmem breaks stuff

--- variants/uno/GPIO.h
+++ variants/uno/GPIO.h
@@ -2,14 +2,13 @@
 #define GPIO_H
 
 #include <avr/io.h>
-#include <avr/pgmspace.h>
 #include <GPIOBase.h>
 
 namespace GPIO
 {
-	const PROGMEM Port B = {&PINB, &DDRB, &PORTB};
-	const PROGMEM Port C = {&PINC, &DDRC, &PORTC};
-	const PROGMEM Port D = {&PIND, &DDRD, &PORTD};
+	const Port B = {&PINB, &DDRB, &PORTB};
+	const Port C = {&PINC, &DDRC, &PORTC};
+	const Port D = {&PIND, &DDRD, &PORTD};
 }
 
 #endif
