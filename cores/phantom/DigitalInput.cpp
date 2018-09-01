62e0b879e2c20dd34b14eb1cad8911e180f857f7
Liam Kinne <kinneliam@gmail.com>
2018-09-01 15:09:48 +1000

    convert to Pin usage over Port and bit

--- cores/phantom/DigitalInput.cpp
+++ cores/phantom/DigitalInput.cpp
@@ -1,14 +1,14 @@
 #include "DigitalInput.h"
 
 DigitalInput::DigitalInput(GPIO::Port port, uint8_t bit):
-	port(port),
+	pin(GPIO::Pin {port, bit}),
 	bitmask(1<<bit)
 {
 	initialize();
 }
 
 DigitalInput::DigitalInput(GPIO::Pin pin):
-	port(pin.port),
+	pin(pin),
 	bitmask(1<<pin.bit)
 {
 	initialize();
@@ -16,15 +16,15 @@ DigitalInput::DigitalInput(GPIO::Pin pin):
 
 void DigitalInput::set_pullup(bool setup) const
 {
-	*port.output_register |= bitmask;
+	*pin.port.output_register |= bitmask;
 }
 
 bool DigitalInput::get() const
 {
-	return *port.input_register & bitmask;
+	return *pin.port.input_register & bitmask;
 }
 
 void DigitalInput::initialize() const
 {
-	*port.mode_register &= ~bitmask;
+	*pin.port.mode_register &= ~bitmask;
 }
