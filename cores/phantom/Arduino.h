77f02a91180d01f0c0036439ec47d1064544c2c8
liamkinne <kinneliam@gmail.com>
2018-09-09 00:52:32 +1000

    put helper include in arduino header

--- cores/phantom/Arduino.h
+++ cores/phantom/Arduino.h
@@ -1,4 +1,6 @@
 /*
-	Dummy File.
 	The Arduino IDE automatically puts an include for Arduino.h at the top of sketches
 */
+
+// This is needed because the Arduino IDE doesn't compile files without extensions (e.g. <vector>)
+#include <StandardCplusplus.h>
