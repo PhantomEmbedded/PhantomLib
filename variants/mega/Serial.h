ecd57fd926dafe2083b0df4e94f8a8813f129df0
liamkinne <kinneliam@gmail.com>
2018-09-25 11:46:01 +1000

    add interfaces for mega

---
+++ variants/mega/Serial.h
@@ -0,0 +1,31 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <SerialDevice.h>
+
+namespace Serial
+{
+	enum Rate : uint32_t
+	{
+		bps2400 = 2400,
+		bps4800 = 4800, 
+		bps9600 = 9600,
+		bps14400 = 14400,
+		bps19200 = 19200,
+		bps28800 = 28800,
+		bps38400 = 38400,
+		bps57600 = 57600,
+		bps76800 = 76800,
+		bps115200 = 115200,
+		bps230400 = 230400,
+		bps250000 = 250000,
+		bps500000 = 500000,
+		bps1000000 = 1000000
+	};
+
+	const Device A = {&UDR0, {&UCSR0A, &UCSR0B, &UCSR0C}, {&UBRR0L, &UBRR0H}};
+
+	SerialDevice main(A, 57600);
+}
+
+#endif
